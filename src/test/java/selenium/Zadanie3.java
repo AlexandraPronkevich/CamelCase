47a1d8005cfdc64435336b6613c011c4805e718b
Alexandra Pronkevich <pronkevich111@gmail.com>
2022-04-14 23:33:00 +0300

    commit

--- src/test/java/selenium/Zadanie3.java
+++ src/test/java/selenium/Zadanie3.java
@@ -4,23 +4,8 @@ public class Zadanie3 {
     public static void main(String[] args) {
 
     String str = ":smile:";
-        "emojiChar": "🤪",
-//    String result = EmojiParser.parseToUnicode(str);
-        System.out.println(🤩);
+        System.out.println("смайлик ;)");
     }
-//    { "emojiChar": "🤪",
-//        "emojiChar": "🤩",
-//            "emoji": "\uD83E\uDD29",
-//            "description": "star-struck",
-//            "aliases": [
-//        "star_struck",
-//                "excited",
-//                "star_eyes",
-//                "starry_eyed",
-//                "wow_face",
-//                "face_with_starry_eyes",
-//                "grinning_face_with_star_eyes"
-//    ],
-//        "tags": []
-    }
+
+
 }
