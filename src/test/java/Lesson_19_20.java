60398063d3e6ee28656066b034837270c9f96bd3
Alexandra Pronkevich <pronkevich111@gmail.com>
2022-05-02 23:08:53 +0300

    commit2

---
+++ src/test/java/Lesson_19_20.java
@@ -0,0 +1,21 @@
+public class Lesson_19_20 {
+    public static void main(String[] args) {
+
+        for (int i = 40; i <= 60; i++)
+            if (i % 4 == 0) {
+                System.out.println(i);
+
+                try {
+                    i = i > 45;
+                } catch (Exception e) {
+                    System.out.println("Число должно быть меньше 45");
+                }
+
+            }
+
+    }
+}
+
+
+
+
